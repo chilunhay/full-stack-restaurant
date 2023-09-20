19de9ebd934dc2d841c2b29444486d0f0ddee515
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-09-20 18:45:05 +0700

    fix menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -21,12 +21,12 @@ const MenuPage = async () => {
         <Link
           href={`/menu/${category.slug}`}
           key={category.id}
-          className="w-full h-1/3 bg-cover p-8 md:h-1/2"
+          className="w-full h-1/3 bg-cover p-4 md:p-8 md:h-[60%]"
           style={{ backgroundImage: `url(${category.img})` }}
         >
-          <div className={`text-${category.color} w-1/2`}>
+          <div className={`text-${category.color} w-1/2 md:w-2/3`}>
             <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
-            <p className="text-sm my-8">{category.desc}</p>
+            <p className="text-sm my-1 md:my-8">{category.desc}</p>
             <button
               className={`hidden 2xl:block bg-${category.color} text-${
                 category.color === 'black' ? 'white' : 'red-500'
