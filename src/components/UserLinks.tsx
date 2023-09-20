c2d5b33173c21ef73a86fbb18b01a7105df450dd
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-09-20 19:26:12 +0700

    fix navbar

--- src/components/UserLinks.tsx
+++ src/components/UserLinks.tsx
@@ -1,16 +1,18 @@
-"use client";
+'use client';
 
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
+import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const UserLinks = () => {
   const { status } = useSession();
   return (
     <div>
-      {status === "authenticated" ? (
+      {status === 'authenticated' ? (
         <div>
           <Link href="/orders">Orders</Link>
-          <span className="ml-4 cursor-pointer" onClick={() => signOut()}>Logout</span>
+          <span className="ml-2 cursor-pointer" onClick={() => signOut()}>
+            Logout
+          </span>
         </div>
       ) : (
         <Link href="/login">Login</Link>
