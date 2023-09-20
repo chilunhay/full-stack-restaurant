027c4b1c6842cb03845365d69ba1a7950c649dd5
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-09-20 17:05:21 +0700

    fix 6

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,23 +1,21 @@
-import { MenuType } from "@/types/types";
-import Link from "next/link";
-import React from "react";
+import { MenuType } from '@/types/types';
+import Link from 'next/link';
+import React from 'react';
 
-const getData = async ()=>{
-  const res = await fetch("http://localhost:3000/api/categories",{
-    cache:"no-store"
-  })
+const getData = async () => {
+  const res = await fetch('api/categories', {
+    cache: 'no-store',
+  });
 
-  if(!res.ok){
-    throw new Error("Failed!");
-    
+  if (!res.ok) {
+    throw new Error('Failed!');
   }
 
-  return res.json()
-}
+  return res.json();
+};
 
 const MenuPage = async () => {
-
-  const menu:MenuType = await getData()
+  const menu: MenuType = await getData();
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
@@ -30,7 +28,13 @@ const MenuPage = async () => {
           <div className={`text-${category.color} w-1/2`}>
             <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
             <p className="text-sm my-8">{category.desc}</p>
-            <button className={`hidden 2xl:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} py-2 px-4 rounded-md`}>Explore</button>
+            <button
+              className={`hidden 2xl:block bg-${category.color} text-${
+                category.color === 'black' ? 'white' : 'red-500'
+              } py-2 px-4 rounded-md`}
+            >
+              Explore
+            </button>
           </div>
         </Link>
       ))}
