1da7c66b9988d8cf2163dde6681a87832b975d08
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-09-20 15:45:16 +0700

    fix 2

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,7 +1,7 @@
-import { prisma } from "@/utils/connect";
-import { NextResponse } from "next/server";
+import { prisma } from '@/utils/connect';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
+export const PUT = async (req: NextRequest, { params }: { params: { intentId: string } }) => {
   const { intentId } = params;
 
   try {
@@ -9,17 +9,11 @@ export const PUT = async ({ params }: { params: { intentId: string } }) => {
       where: {
         intent_id: intentId,
       },
-      data: { status: "Being prepared!" },
+      data: { status: 'Being prepared!' },
     });
-    return new NextResponse(
-      JSON.stringify({ message: "Order has been updated" }),
-      { status: 200 }
-    );
+    return new NextResponse(JSON.stringify({ message: 'Order has been updated' }), { status: 200 });
   } catch (err) {
     console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }),
-      { status: 500 }
-    );
+    return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }), { status: 500 });
   }
 };
